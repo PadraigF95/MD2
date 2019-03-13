c3f51d603cc4b8d5a461e67b6b1bb2ad7e2dcaaf
PadraigF95 <44588037+PadraigF95@users.noreply.github.com>
2019-03-13 00:32:13 +0000

    Update Favourites.java

--- Tourism/app/src/main/java/ie/wit/tourism/activities/Favourites.java
+++ Tourism/app/src/main/java/ie/wit/tourism/activities/Favourites.java
@@ -20,21 +20,5 @@ public class Favourites extends Base {
         emptyList = findViewById(R.id.emptyList);
     }
 
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if(app.attractionList.isEmpty())
-            emptyList.setText(getString(R.string.emptyMessageLbl));
-        else
-            emptyList.setText("");
-
-        attractionFragment = AttractionFragment.newInstance(); //get a new Fragment instance
-        getFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, attractionFragment)
-                .commit(); // add it to the current activity
-    }
-
 }
 
